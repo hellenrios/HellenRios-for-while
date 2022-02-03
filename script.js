1d233e7f25b53f0ad67e803a6f1f4896b0e248b2
Hellen Rios <hellenrioss@gmail.com>
2022-02-03 13:05:44 -0300

    adicionando exercicio de for/while

---
+++ script.js
@@ -0,0 +1,26 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+let x = 10
+
+while (x >= 1){
+    console.log(x)
+    x--
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+
+for(let i = 1; i <= 100; i += 2){
+    console.log(i);
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+for(let i = 0; i <= 100; i+= 2){
+    console.log(i)
+}
